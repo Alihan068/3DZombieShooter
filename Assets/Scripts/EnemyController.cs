4208db5c767652fd812e69d5aec613a6f1a23682
Alihan <alihankomurcuoglu@gmail.com>
2025-10-22 18:51:57 +0300

    Improvements
    TODO fix zombie movement

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections;
+using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -12,10 +13,12 @@ public enum EnemyState {
 
 public class EnemyController : MonoBehaviour {
 
-    [SerializeField] float chaseRange = 10f;
-    [SerializeField] float chaseTimeout = 2f;
+    [SerializeField] float viewRange = 10f;
+    [SerializeField] float chaseTimeout = 10f;
+    [SerializeField] float investigateTimeout = 5f;
 
     [SerializeField] float turnSpeed = 5f;
+    float outOfInvestigationTimer = 0f;
     float outOfRangeTimer = 0f;
     bool isProvoked = false;
 
@@ -33,13 +36,16 @@ public class EnemyController : MonoBehaviour {
 
     float distanceToTarget = Mathf.Infinity;
 
+    [SerializeField] float hearingDistance = 10f;
+    [SerializeField] float InvestigationTime = 5f;
+    [SerializeField] float fovLimit = 90f;
     void Start() {
         navMeshAgent = GetComponent<NavMeshAgent>();
         enemyHealth = GetComponent<EnemyHealth>();
         target = FindFirstObjectByType<PlayerHealth>().transform;
         audioSource = GetComponent<AudioSource>();
         StartCoroutine(PlaySoundAfterDelay(10f, idleSounds));
-        enemyState = GetComponent<EnemyState>();
+        enemyState = EnemyState.Idle;
     }
 
     void Update() {
@@ -49,22 +55,94 @@ public class EnemyController : MonoBehaviour {
         }
         distanceToTarget = Vector3.Distance(transform.position, target.position);
 
-        if (distanceToTarget >= chaseRange) {
-            outOfRangeTimer += Time.deltaTime;
+        //if (distanceToTarget >= viewRange) {
+        //    outOfRangeTimer += Time.deltaTime;
 
-            if (outOfRangeTimer >= chaseTimeout) {
-                StopChase();
-            }
-        }
-        else if (isProvoked) {
-            EngageTarget();
-        }
-        else if (distanceToTarget <= chaseRange) {
-            isProvoked = true;
+        //    if (outOfRangeTimer >= chaseTimeout) {
+        //        StopChase();
+        //    }
+        //}
+        //else if (isProvoked) {
+        //    EngageTarget();
+        //}
+        //else if (distanceToTarget <= viewRange) {
+        //    isProvoked = true;
+        //}
+
+        switch (enemyState) {
+            case EnemyState.Idle:
+                IdleBehaviour();
+                if (isProvoked || distanceToTarget <= hearingDistance) {
+                    Debug.Log("From Idle to Ivnest");
+                    enemyState = EnemyState.Investigating;
+                    
+                }
+                break;
+
+            case EnemyState.Investigating:
+                FaceTarget();
+                Debug.Log("Investigate");
+                outOfInvestigationTimer += Time.deltaTime;
+
+                if (CanSeePlayer()) {
+                    Debug.Log("From Invest to Chase");
+                    enemyState = EnemyState.Chasing;
+                    
+                }
+
+                else if (outOfInvestigationTimer >= investigateTimeout) {
+                    Debug.Log("From Chase To Invest");
+                    enemyState = EnemyState.Idle;
+                    
+                }
+
+                
+                break;
+
+            case EnemyState.Chasing:
+
+                ChaseTarget();
+
+                if (distanceToTarget <= navMeshAgent.stoppingDistance) {
+                    enemyState = EnemyState.Attacking;
+                }
+                else if (outOfRangeTimer >= chaseTimeout) {
+                    StopChase();
+                    enemyState = EnemyState.Investigating;
+                }
+                else if (distanceToTarget >= viewRange) {
+                    outOfRangeTimer += Time.deltaTime;
+                }
+                
+
+                break;
+
+            case EnemyState.Attacking:
+
+                FaceTarget();
+                AttackTarget();
+                if (distanceToTarget >= navMeshAgent.stoppingDistance) {
+                    enemyState = EnemyState.Chasing;
+                }
+                break;
         }
 
     }
+    bool CanSeePlayer() {
 
+        if (distanceToTarget <= viewRange) {
+
+            Vector3 directionToTarget = (target.position - transform.position).normalized;
+            float angleBetweenTarget = Vector3.Angle(Vector3.forward, directionToTarget);
+
+            if (angleBetweenTarget < fovLimit / 2) {
+                if (!Physics.Linecast(transform.position + Vector3.up, target.position + Vector3.up)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 
     void EngageTarget() {
         FaceTarget();
@@ -77,34 +155,26 @@ public class EnemyController : MonoBehaviour {
             AttackTarget();
         }
     }
-    bool CanSeePlayer() {
-        if (enemyHealth.IsDead()) return false;
 
-        if (distanceToTarget <= viewRadius) {
-            Vector3 directionToTarget = (target.position - transform.position).normalized;
+    void IdleBehaviour() {
+        Debug.Log("Idle");
+        StartCoroutine(PlaySoundAfterDelay(10f, idleSounds));
+        outOfInvestigationTimer = 0;
+        outOfRangeTimer = 0;
 
-            float angleBetweenEnemyAndTarget = Vector3.Angle(transform.forward, directionToTarget);
-
-            if (angleBetweenEnemyAndTarget <= viewAngle / 2) {
-                if (!Physics.Linecast(transform.position + Vector3.up * 3f,
-                    target.position + Vector3.up * 3f, obstacleMask)) {
-                    return true;
-                }
-            }
-        }
-        return false;
     }
-
     void ChaseTarget() {
-        enemyState = EnemyState.Chasing;
+        Debug.Log("Chase!");
+        outOfInvestigationTimer = 0;
         StartCoroutine(PlaySoundAfterDelay(10f, chaseSounds));
         navMeshAgent.isStopped = false;
         GetComponent<Animator>().SetBool("attack", false);
         GetComponent<Animator>().SetBool("isMoving", true);
         navMeshAgent.SetDestination(target.position);
     }
+
     void StopChase() {
-        enemyState = EnemyState.Idle;
+        Debug.Log("StopChase");
         GetComponent<Animator>().SetBool("isMoving", false);
         isProvoked = false;
         navMeshAgent.ResetPath();
@@ -116,7 +186,7 @@ public class EnemyController : MonoBehaviour {
             soundPlayedOnce = true;
             yield return new WaitForSeconds(0.1f);
             audioSource.PlayOneShot(clip[UnityEngine.Random.Range(0, clip.Length)]);
-            yield return new WaitForSeconds(delay);
+            yield return new WaitForSeconds(UnityEngine.Random.Range(5f, delay));
             soundPlayedOnce = false;
         }
     }
@@ -136,8 +206,15 @@ public class EnemyController : MonoBehaviour {
 
     private void OnDrawGizmos() {
         Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(transform.position, chaseRange);
+        Gizmos.DrawWireSphere(transform.position, viewRange);
+        Vector3 sightAngleLeft = DirectionFromAngle(-fovLimit / 2);
+        Vector3 sightAngleRight = DirectionFromAngle(fovLimit / 2);
+        Gizmos.DrawLine(transform.position, transform.position + sightAngleLeft * viewRange);
+        Gizmos.DrawLine(transform.position, transform.position + sightAngleRight * viewRange);
+    }
 
+    Vector3 DirectionFromAngle(float angleInDegrees) {
+        return Quaternion.Euler(0, transform.eulerAngles.y + angleInDegrees, 0) * Vector3.forward;
     }
 
     //private void OldUpdateVersion() {
