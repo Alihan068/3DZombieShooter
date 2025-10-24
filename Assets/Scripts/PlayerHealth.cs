d0a1bcfb456b97e89e8a76726b3cb5886157c17d
Alihan <alihankomurcuoglu@gmail.com>
2025-10-24 13:41:23 +0300

    Improvements
    Collectible glow
    HpBar
    Uı improvements

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,11 +1,17 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
     [SerializeField] float healthPoints = 100f;
-   
+    [SerializeField] Slider healthSlider;
+    private void Start() {
+        healthSlider.maxValue = healthPoints;
+        healthSlider.value = healthPoints;
+    }
     public void TakeDamage(float damage) {
         healthPoints -= damage;
+        healthSlider.value = healthPoints;
         Debug.Log(this.name + "took damage, remaining hp: " + healthPoints);
         if(healthPoints <= 0 ) {
             GetComponent<DeathHandler>().HandleDeath();
